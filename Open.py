8912bbc85244f516482e0ef7a7704eeb428c76aa
wisdomfri <156893861+wisdomfri@users.noreply.github.com>
2024-07-17 15:50:12 +0100

    Create Open.py

---
+++ Open.py
@@ -0,0 +1,1 @@
+mport  cv2haar_cas=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')image =cv2.imread('down.jpg')gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)faces = haar_cas.detectMultiScale(gray,1.1,4)for(x,y,w,h) in faces:    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),5)     cv2.imshow("Gray",image)cv2.waitKey()
